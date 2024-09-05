1f88553129c8c84157dca032fb1bc849e5a2babb
Anurag6523 <156291399+Anurag6523@users.noreply.github.com>
2024-09-05 17:06:57 +0530

    middleware start

--- index.js
+++ index.js
@@ -2,18 +2,35 @@ const express=require("express");
 const app=express();
 
 
-app.use(express.json());  //jsonData->object
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());  
+app.use(express.urlencoded({extended:true}));
+// app.use(express.text());// text->String
 
 
 app.post("/",(req,res)=>{
     res.send("post request home")
 })
 
-app.post("/user",(req,res)=>{
-    // console.log(req.query)
-    console.log(req.body)
-   res.send("Hello World");
+const m1=(req,res,next)=>{
+    req.city="mohaliiiiiiii";
+    console.log("middleware1");
+    next();
+}
+
+const m2=(req,res,next)=>{
+    console.log("middleware2");
+    res.send("hii middleware 2");
+    
+    next();
+}
+
+
+
+app.post("/user",m1,m2,(req,res)=>{
+ 
+    console.log(req.body);
+    console.log(req)
+//    res.send("Hello World");
 });
 
 app.get("/user/:name",(req,res)=>{     
